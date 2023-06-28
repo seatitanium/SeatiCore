a5021f8ee6dd253b1f4e5c180304b9de73d29867
Subilan <suyuanliang1995@gmail.com>
2023-06-28 22:12:38 +0800

    initial

---
+++ src/main/java/cc/seati/seatic/Main.java
@@ -0,0 +1,19 @@
+package cc.seati.seatic;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+import org.slf4j.Logger;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "seatic";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
