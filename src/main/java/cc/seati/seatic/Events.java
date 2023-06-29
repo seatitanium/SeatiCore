5083cf73cb3d5a4d1bf0b694ebedc226e9057336
Subilan <suyuanliang1995@gmail.com>
2023-06-30 00:51:28 +0800

    feat: get server instance available using events

---
+++ src/main/java/cc/seati/seatic/Events.java
@@ -0,0 +1,27 @@
+package cc.seati.seatic;
+
+import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class Events {
+    @SubscribeEvent
+    public static void serverStarted(ServerStartedEvent e) {
+        SeatiCore.server = e.getServer();
+        SeatiCore.ready = true;
+        Utils.log.info("Got server instance.");
+    }
+
+    @SubscribeEvent
+    public static void serverStarting(ServerStartingEvent e) {
+        Utils.log.info("""
+                   _____            __  _\s
+                  / ___/___  ____ _/ /_(_)
+                  \\__ \\/ _ \\/ __ `/ __/ /\s
+                 ___/ /  __/ /_/ / /_/ / \s
+                /____/\\___/\\__,_/\\__/_/  \s
+                                         \s
+                """);
+        Utils.log.info("SeatiCore - v1.0.0");
+    }
+}
--- src/main/java/cc/seati/seatic/SeatiCore.java
+++ src/main/java/cc/seati/seatic/SeatiCore.java
@@ -1,19 +1,39 @@
 package cc.seati.seatic;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkConstants;
 import org.slf4j.Logger;
 
-@Mod(Main.MODID)
-public class Main {
+@Mod(SeatiCore.MODID)
+public class SeatiCore {
     public static final String MODID = "seatic";
+    public static final Log LOG = new Log();
+    public static int uptime = 0;
+    public static boolean ready = false;
+    public static MinecraftServer server;
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Main() {
-        LOGGER.info(System.getProperty("user.dir"));
+    @SuppressWarnings("BusyWait")
+    private static final Thread timerThread = new Thread(() -> {
+        while(true) {
+            uptime++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    });
+
+    public SeatiCore() {
+        timerThread.start();
+        MinecraftForge.EVENT_BUS.register(Events.class);
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
